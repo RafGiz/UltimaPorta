fddd2eba27c4820d68df4b6f0fce8d5a6a4c831e
APPLE <apple@APPLEs-iMac-2.local>
2023-02-14 12:45:29 +0100

    Security implementation

--- src/main/java/pl/codecoolproject/ultimaporta/Init.java
+++ src/main/java/pl/codecoolproject/ultimaporta/Init.java
@@ -2,10 +2,11 @@ package pl.codecoolproject.ultimaporta;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
-import pl.codecoolproject.ultimaporta.model.Departed;
-import pl.codecoolproject.ultimaporta.model.ObituaryNotice;
+import pl.codecoolproject.ultimaporta.model.*;
 import pl.codecoolproject.ultimaporta.repository.DepartedRepository;
 import pl.codecoolproject.ultimaporta.repository.ObituaryNoticeRepository;
+import pl.codecoolproject.ultimaporta.repository.RoleRepository;
+import pl.codecoolproject.ultimaporta.repository.UserRepository;
 
 import javax.annotation.PostConstruct;
 import java.time.LocalDate;
@@ -18,8 +19,21 @@ public class Init {
 
     private final ObituaryNoticeRepository obituaryNoticeRepository;
 
+    private UserRepository userRepository;
+
+    private RoleRepository roleRepository;
+
+
     @PostConstruct
     public void runList(){
+
+        User user1 = new User("mateo1","mateopadre@gmail.com","password");
+        userRepository.save(user1);
+
+        Role role1 = new Role(ERole.ROLE_MODERATOR);
+        roleRepository.save(role1);
+
+
 
         Departed departed1 = new Departed(1l,"Jan", "Kowalski", LocalDate.of(2023,2,3));
         departedRepository.save(departed1);
--- src/main/java/pl/codecoolproject/ultimaporta/controller/DepartedController.java
+++ src/main/java/pl/codecoolproject/ultimaporta/controller/DepartedController.java
@@ -1,12 +1,11 @@
 package pl.codecoolproject.ultimaporta.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import pl.codecoolproject.ultimaporta.model.Departed;
-import pl.codecoolproject.ultimaporta.service.DepartedService;
+import pl.codecoolproject.ultimaporta.security.service.DepartedService;
 
 import java.util.List;
 
