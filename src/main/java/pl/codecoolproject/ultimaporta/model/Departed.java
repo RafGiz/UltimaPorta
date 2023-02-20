3e85f035f32b3aa6033211e6d825cb8e7b1a8bcf
damianstrag <damian_jan@onet.eu>
2023-02-20 13:38:02 +0100

    Create ObituatyNoticeController and RepresentOfDeathController

--- src/main/java/pl/codecoolproject/ultimaporta/model/Departed.java
+++ src/main/java/pl/codecoolproject/ultimaporta/model/Departed.java
@@ -19,12 +19,14 @@ import java.time.LocalDate;
 @AllArgsConstructor
 public class Departed {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String name;
     @JsonProperty("lastname")
     private String lastName;
+
     @DateTimeFormat(pattern = "YYYY-MM-DD")
+    @JsonProperty("dateofdeath")
     private LocalDate dateOfDeath;
 
 }
--- src/main/java/pl/codecoolproject/ultimaporta/repository/RepresentOfTheDeathRepository.java
+++ src/main/java/pl/codecoolproject/ultimaporta/repository/RepresentOfTheDeathRepository.java
@@ -3,5 +3,5 @@ package pl.codecoolproject.ultimaporta.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pl.codecoolproject.ultimaporta.model.RepresentOfTheDeath;
 
-public interface ReresentOfTheDeathRepository extends JpaRepository<RepresentOfTheDeath,Long> {
+public interface RepresentOfTheDeathRepository extends JpaRepository<RepresentOfTheDeath,Long> {
 }
