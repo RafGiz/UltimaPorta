ff663680e35107d2d2da672e612a7368140eb8b4
rafgi <rafgizinski@gmail.com>
2023-02-22 21:20:33 +0100

    background, icons, fonts

--- src/webapp/src/components/Offer.tsx
+++ src/webapp/src/components/Offer.tsx
@@ -5,6 +5,8 @@ import urnImage from "./images/urn.png";
 import carImage from "./images/car.png";
 import wreathImage from "./images/wreath.png";
 import clothesImage from "./images/clothes.png";
+import '../background.css';
+// import backgroundImage from "./images/background.png";
 
 export default function Offer() {
   const [burialVisible, setBurialVisible] = useState(false);
@@ -25,68 +27,155 @@ export default function Offer() {
         id="offer-text"
         style={{
           textAlign: "center",
-          fontFamily: "Arial, sans-serif",
-          fontSize: "36px",
+          fontFamily: "Gloock, sans-serif",
+          fontSize: "46px",
           marginTop: "50px",
         }}
       >
-        Nasza Oferta
+        <u><b>Nasza Oferta</b></u>
       </div>
       <div id="offer-subtext">
         <div>
-          <button onClick={handleBurialClick}>Organizacja Pogrzebu</button>
+          <button onClick={handleBurialClick}>
+            <div
+              id="offer-text"
+              style={{
+                fontFamily: "Gloock, sans-serif",
+                fontSize: "25px",
+              }}
+            >
+              <u><b>Organizacja Pogrzebu</b></u>
+            </div>
+          </button>
           <div className={burialVisible ? "visible" : ""}>
-            <p>Information about burial</p>
+          <div
+              id="offer-text"
+              style={{
+                marginTop: '20px',
+                fontFamily: "Gloock, sans-serif",
+                fontSize: "20px",
+              }}
+            >
+              Wszelkie informacje odnośnie pogrzebu
+            </div>
           </div>
         </div>
         <div>
-          <button onClick={handleCremationClick}>Organizacja Kremacji</button>
+          <button onClick={handleCremationClick}>
+            <div
+              id="offer-text"
+              style={{
+                fontFamily: "Gloock, sans-serif",
+                fontSize: "25px",
+              }}
+            >
+              <u><b>Organizacja Kremacji</b></u>
+            </div>
+          </button>
           <div className={cremationVisible ? "visible" : ""}>
-            <p>Information about cremation</p>
+          <div
+              id="offer-text"
+              style={{
+                marginTop: '20px',
+                fontFamily: "Gloock, sans-serif",
+                fontSize: "20px",
+              }}
+            >
+              Wszelkie informacje odnośnie kremacji
+            </div>
           </div>
         </div>
       </div>
       <div style={{ display: "flex" }}>
-        <div style={{ marginRight: "50px",marginTop: "300px" }}>
+        <div style={{ marginRight: "50px", marginTop: "300px" }}>
           <img
             src={coffinImage}
             alt="Coffin"
             style={{ width: "200px", height: "200px" }}
           />
-          <p>Coffins</p>
+          <div
+            id="offer-text"
+            style={{
+              textAlign: "center",
+              marginTop: "20px",
+              fontFamily: "Gloock, sans-serif",
+              fontSize: "23px",
+            }}
+          >
+            Trumny
+          </div>
         </div>
-        <div style={{ marginRight: "50px",marginTop: "300px" }}>
+        <div style={{ marginRight: "50px", marginTop: "300px" }}>
           <img
             src={urnImage}
             alt="Urns"
             style={{ width: "200px", height: "200px" }}
           />
-          <p>Urns</p>
+          <div
+            id="offer-text"
+            style={{
+              textAlign: "center",
+              marginTop: "20px",
+              fontFamily: "Gloock, sans-serif",
+              fontSize: "23px",
+            }}
+          >
+            Urny
+          </div>
         </div>
-        <div style={{ marginRight: "50px",marginTop: "300px" }}>
+        <div style={{ marginRight: "50px", marginTop: "300px" }}>
           <img
             src={carImage}
             alt="Cars"
             style={{ width: "200px", height: "200px" }}
           />
-          <p>Cars</p>
+          <div
+            id="offer-text"
+            style={{
+              textAlign: "center",
+              marginTop: "20px",
+              fontFamily: "Gloock, sans-serif",
+              fontSize: "23px",
+            }}
+          >
+            Samochody
+          </div>
         </div>
-        <div style={{ marginRight: "50px",marginTop: "300px" }}>
+        <div style={{ marginRight: "50px", marginTop: "300px" }}>
           <img
             src={wreathImage}
             alt="Funeral Wreaths"
             style={{ width: "200px", height: "200px" }}
           />
-          <p>Funeral Wreaths</p>
+          <div
+            id="offer-text"
+            style={{
+              textAlign: "center",
+              marginTop: "20px",
+              fontFamily: "Gloock, sans-serif",
+              fontSize: "23px",
+            }}
+          >
+            Wieńce
+          </div>
         </div>
         <div>
-          
           <img
             src={clothesImage}
             alt="Funeral Clothes"
-            style={{ width: "200px", height: "200px",marginTop: "300px" }}
+            style={{ width: "200px", height: "200px", marginTop: "300px" }}
           />
-          <p>Funeral Clothes</p>
+          <div
+            id="offer-text"
+            style={{
+              textAlign: "center",
+              marginTop: "20px",
+              fontFamily: "Gloock, sans-serif",
+              fontSize: "23px",
+            }}
+          >
+            Odzież pogrzebowa
+          </div>
         </div>
       </div>
     </div>
