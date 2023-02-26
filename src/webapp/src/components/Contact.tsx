56690f9c67d94d7da7f46c7786d57bae0c857c4b
rafgi <rafgizinski@gmail.com>
2023-02-26 01:27:32 +0100

    mamy to

--- src/webapp/src/components/Contact.tsx
+++ src/webapp/src/components/Contact.tsx
@@ -1,10 +1,42 @@
+import FacebookIcon from '@mui/icons-material/Facebook';
+import PhoneAndroidIcon from '@mui/icons-material/PhoneAndroid';
+import HomeWorkIcon from '@mui/icons-material/HomeWork';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import { Box } from '@mui/system';
 
 export default function Contact() {
   return(
     <div className="card">
         <span className="font-link">
-            Kontakt
+            <h1>Kontakt</h1>
+            <div style={{textAlign: 'center', fontSize: 23}}>
+            Zapraszamy do kontaktu z naszym biurem:
+            
+            <center><HomeWorkIcon/>Ulica Cmentarna 66/6</center>
+            80-441 Warszawa
+            </div>
+            <br/>
+            <div style={{textAlign: 'center', fontSize: 23}}>
+                <PhoneAndroidIcon/>666 665 664
+            </div>
+            <br/>
+            <div style={{textAlign: 'center', fontSize: 23}}>
+                <FacebookIcon/> UltimaPorta.pl
+            </div>
+            <br/>
+            <div style={{textAlign: 'center', fontSize: 23}}>
+                <LinkedInIcon/> UltimaPorta.LinkedIn.pl
+            </div>
+            <br/>
+            
+            <h1>Zapraszamy!</h1>
+
+            
+
+            
         </span>
     </div>
+    
+
 )
 };
